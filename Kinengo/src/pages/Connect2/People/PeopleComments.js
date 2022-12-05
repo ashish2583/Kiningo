2cbe167ba8ec30ed533d5d19bd4715e1415d332a
Saurabh Saneja <saurabh250183@gmail.com>
2022-12-05 16:48:02 +0530

    minor

--- Kinengo/src/pages/Connect2/People/PeopleComments.js
+++ Kinengo/src/pages/Connect2/People/PeopleComments.js
@@ -18,47 +18,112 @@ const PeopleComments = (props) => {
   const [multiSliderValue, setMultiSliderValue] = useState([0, 100])
   const [showChooseMilesModal, setShowChooseMilesModal] = useState(false)
   const [userMessage, setUserMessage] = useState('')
+  const [replyingTo, setReplyingTo] = useState('')
   const [upData,setupData]=useState([
     {
       id: '1',
-      name: 'Aryav Nadkarni',
+      name: 'Maude Hall',
       message:`That's a fantastic new app feature. You and your team did an excellent job of incorporating user testing feedback.`,
       time:'14 min',
       img:require('../../../assets/comment-person-image.png'),
-      isLiked: true
+      isLiked: true,
+      replies:[]
     },
     {
       id: '2',
-      name: 'Aryav Nadkarni',
+      name: 'Eleanor Pena',
       message:`That's a fantastic new app feature. You and your team did an excellent job of incorporating user testing feedback.`,
       time:'14 min',
       img:require('../../../assets/comment-person-image.png'),
-      isLiked: false
+      isLiked: false,
+      replies:[]
     },
     {
       id: '3',
-      name: 'Aryav Nadkarni',
+      name: 'Floyd Miles',
       message:`That's a fantastic new app feature. You and your team did an excellent job of incorporating user testing feedback.`,
       time:'14 min',
       img:require('../../../assets/comment-person-image.png'),
-      isLiked: true
+      isLiked: true,
+      replies:[]
     },
     {
       id: '4',
-      name: 'Aryav Nadkarni',
+      name: 'Robert Fox',
       message:`That's a fantastic new app feature. You and your team did an excellent job of incorporating user testing feedback.`,
       time:'14 min',
       img:require('../../../assets/comment-person-image.png'),
-      isLiked: true
+      isLiked: true,
+      replies:[]
     },
 
   ])
   const multiSliderValuesChange = (values) => {setMultiSliderValue(values)}
   useEffect(()=>{
+    console.log('upData changed', upData);
+ },[upData])
 
- },[])
+ const sendMessage = () => {
+  const upDataCopy = [...upData]
+  upDataCopy.map(el=>{
+    if(replyingTo === el.id){
+      el.replies.push({
+        id:99,
+        name:'saurabh saneja',
+        message: userMessage,
+        time: '0 min',
+        img: require('../../../assets/people-sender-image.png'),
+        isLiked: false
+      })
+      return el
+    }
+  })
+  setupData([...upDataCopy])
+  setUserMessage('')
+ }
+ const likeChildComment = (parentId, childIndex) => {
+  const upDataCopy = [...upData]
+  upDataCopy.map(el => {
+    if(el.id === parentId){
+      el.replies[childIndex].isLiked = !el.replies[childIndex].isLiked
+    }
+    return el
+  })
+  setupData([...upDataCopy])
+  // setupData(upData.map((el, elIndex)=> index === elIndex ? {...el, isLiked: !item.isLiked} : el))
+ }
 
-
+ const returnReplies = (itemid) => {
+  const replies = upData?.find(el=>el.id === itemid)?.replies
+  return (
+    replies?.map((item, index)=> 
+    <>
+    <View style={{width:'80%', marginLeft:30, marginTop:10}}>
+    <View style={{flexDirection:'row', alignItems:'center'}}>
+      <Image source={item.img}/>
+      <Text style={{fontSize:18, fontWeight:'500', color:'#000', marginLeft:10}}>{item.name}</Text>
+      <Text style={{fontSize:12, fontWeight:'400', color:'#B4BBC6', marginLeft:20}}>{item.time}</Text>
+    </View>
+    <View style={{marginTop:10}}>
+      <Text style={{fontSize:14, fontWeight:'400', color:'#272727'}}>{item.message}</Text>
+    </View>
+    <View style={{marginTop:15, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+      <View style={{flexDirection:'row', alignItems:'center'}}>
+        <TouchableOpacity onPress={()=>{likeChildComment(itemid, index)}}>
+          <Image source={item.isLiked ? require('../../../assets/people-unsel-heart.png') : require('../../../assets/people-sel-heart.png')} style={{width:30, height:30}}/>
+        </TouchableOpacity>
+        <Text style={{fontSize:14, fontWeight:'500', color:'#B4BBC6', marginLeft:10}}>Like</Text>
+      </View>
+      <TouchableOpacity onPress={()=>{myTextInput.current.focus(); setUserMessage(`@${item.name}`); setReplyingTo(itemid)}} style={{flexDirection:'row', alignItems:'center'}}>
+        <Image source={require('../../../assets/people-reply-image.png')}/>
+        <Text style={{fontSize:14, fontWeight:'500', color:'#B4BBC6', marginLeft:10}}>Reply</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+    </>
+    )
+    )
+ }
   return(
     <SafeAreaView scrollEnabled={scrollEnabled} style={{backgroundColor:'#fff'}}>
       <ScrollView>
@@ -77,6 +142,7 @@ const PeopleComments = (props) => {
                   renderItem={({item,index})=>{
                     return(
                       <View style={{width:dimensions.SCREEN_WIDTH*0.9,marginHorizontal:5, marginBottom:15, paddingHorizontal:20}}>
+                        <>
                         <View>
                           <View style={{flexDirection:'row', alignItems:'center'}}>
                             <Image source={item.img}/>
@@ -93,13 +159,19 @@ const PeopleComments = (props) => {
                               </TouchableOpacity>
                               <Text style={{fontSize:14, fontWeight:'500', color:'#B4BBC6', marginLeft:10}}>Like</Text>
                             </View>
-                            <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <TouchableOpacity onPress={()=>{myTextInput.current.focus(); setUserMessage(`@${item.name}`); setReplyingTo(item.id)}} style={{flexDirection:'row', alignItems:'center'}}>
                               <Image source={require('../../../assets/people-reply-image.png')}/>
                               <Text style={{fontSize:14, fontWeight:'500', color:'#B4BBC6', marginLeft:10}}>Reply</Text>
-                            </View>
+                            </TouchableOpacity>
                           </View>
                         </View>
                         <View style={{borderBottomColor: '#E0E0E0', borderBottomWidth: 1, marginTop:10}}/>
+                        {item?.replies?.length > 0 ?
+                        <>
+                        {returnReplies(item.id)}
+                        </>
+                        :null}
+                        </>
                      </View>
                     )
                   }}
@@ -113,6 +185,7 @@ const PeopleComments = (props) => {
 </ScrollView>
 <View style={styles.addCommentView}>
   <TextInput
+    ref={myTextInput}
     value={userMessage}
     onChangeText={(text) => {
       setUserMessage(text)
@@ -122,7 +195,7 @@ const PeopleComments = (props) => {
     style={styles.input}
     multiline
   />
-  <TouchableOpacity style={styles.sendButtonView}>
+  <TouchableOpacity onPress={sendMessage} style={styles.sendButtonView}>
     <Text style={{fontSize:14, fontWeight:'500', color:'#fff'}}>Send</Text>
   </TouchableOpacity>
   </View>
