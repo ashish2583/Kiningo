211671186a65f754048020c47fb7c7abae8a3f63
Saurabh Saneja <saurabh250183@gmail.com>
2022-12-06 14:00:12 +0530

    minor

--- Kinengo/src/component/HomeHeaderRoundBottom.js
+++ Kinengo/src/component/HomeHeaderRoundBottom.js
@@ -24,7 +24,7 @@ const HomeHeaderRoundBottom = (props) => {
             height:3
             }, 
             shadowRadius: 5,
-            shadowOpacity: 0.03,
+            shadowOpacity: 1,
             elevation: 5,
             }}
             >
