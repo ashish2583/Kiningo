8494fa892b44504010b9fe92d3300224a371cafa
Saurabh Saneja <saurabh250183@gmail.com>
2022-12-06 11:03:43 +0530

    minor

--- Kinengo/src/pages/Connect2/People/PeopleMessage.js
+++ Kinengo/src/pages/Connect2/People/PeopleMessage.js
@@ -94,11 +94,11 @@ const PeopleMessage = (props) => {
                   numColumns={1}
                   renderItem={({item,index})=>{
                     return(
-                      <View style={{width:dimensions.SCREEN_WIDTH,marginHorizontal:5, marginBottom:20}}>
+                      <View style={{width:'100%',marginHorizontal:5, marginBottom:20}}>
                         <View style={{flexDirection:'row', width:'80%', alignSelf: item.me ? 'flex-end' : 'flex-start'}}>
                           <Image source={item.me ? image2: image1} style={{width:30, height:30}}/>
-                          <View style={{width:'80%'}}>
-                            <View style={{backgroundColor:'#fff', marginLeft:10, padding:10, borderRadius:15, marginRight:'auto'}}>
+                          <View style={{width:'100%'}}>
+                            <View style={{width:'85%',backgroundColor:'#fff', marginLeft:10, padding:10, borderRadius:15, marginRight:'auto'}}>
                               <Text style={{fontSize:13, fontWeight:'400', color:'#455A64'}}>{item.message}</Text>
                             </View>
                             <Text style={{fontSize:10, fontWeight:'400', color:'#B2B7B9', marginLeft:10, marginTop:2}}>{item.time}</Text>
