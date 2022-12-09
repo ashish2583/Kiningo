5798f199203a834dc35042534e547224a0d9a450
Saurabh Saneja <saurabh250183@gmail.com>
2022-12-09 14:00:30 +0530

    minor

--- Kinengo/src/pages/Connect2/People/PeopleProfileScreen.js
+++ Kinengo/src/pages/Connect2/People/PeopleProfileScreen.js
@@ -109,6 +109,7 @@ const PeopleProfileScreen = (props) => {
       const resp = await createThumbnail({
         url: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`,
         timeStamp: 10000,
+        cacheName: `http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`
       })
       setVideoThumb(resp.path)
       setupData(el=>el.map(el=>{
