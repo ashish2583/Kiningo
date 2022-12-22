ee93ce1db8dec64c8ebcae211f149842b581216b
Saurabh Saneja <saurabh250183@gmail.com>
2022-12-22 13:09:02 +0530

    minor

--- Kinengo/src/pages/Connect2/Dating/DatingYourMatches.js
+++ Kinengo/src/pages/Connect2/Dating/DatingYourMatches.js
@@ -149,7 +149,7 @@ const styles = StyleSheet.create({
     width:'100%',
     height:'100%',
     alignSelf:'center',
-    borderRadius:7
+    borderRadius:15
   },
   absoluteView:{
     position:'absolute',
